fed6656d0cf4f01668d2eab92d4e84c5f23c4cdd
vinodpatil2002 <vinodnspatil@gmail.com>
2024-03-11 02:12:57 +0530

    Project server setup

---
+++ server/server.ts
@@ -0,0 +1,8 @@
+import {app} from './app';
+require('dotenv').config();
+
+// create server
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
